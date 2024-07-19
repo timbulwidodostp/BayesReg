5094b6d520cb4aa391cc3e58614163ca1a12f324
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-07-19 08:50:49 +0700

    Use BayesReg (bayess) With (In) R Software

---
+++ BayesReg/BayesReg.R
@@ -0,0 +1,14 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Bayesian linear regression output Use BayesReg (bayess) With (In) R Software
+install.packages("bayess")
+library("bayess")
+BayesReg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/BayesReg/main/BayesReg/BayesReg.csv",sep = ";")
+# Estimation Bayesian linear regression output Use BayesReg (bayess) With (In) R Software
+BayesReg(BayesReg$eruptions,BayesReg$waiting)
+# Bayesian linear regression output Use BayesReg (bayess) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
